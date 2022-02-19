695363d1e3de19923c2b9b22c65d0168d3678942
Kyler Best <kyler.b@live.com>
2022-02-18 21:22:07 -0600

    commit tree 6e5f07daba125836ad65b111eac2ce8bf8419aea

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+def line line
+    return puts "The line is currently empty." if line.empty?
+    puts "The line is currently:" + (line.map.with_index(1) {|name, i| " #{i}. #{name}"}).join
+end
+def take_a_number line, newGuy
+    line.push(newGuy)
+    puts "Welcome, #{newGuy}. You are number #{line.length} in line."
+end
+def now_serving line
+    return puts "There is nobody waiting to be served!" if line.empty?
+    puts "Currently serving #{line[0]}."
+    line.shift
+end
